40d1b46d082bb31cde5384b3c60b01bd3bcf7c2f
prashant2150487 <swepra04@gmail.com>
2026-05-14 17:40:00 +0530

    feat: craete huddle loop

---
+++ udemyPython/day6/callFunction.py
@@ -0,0 +1,20 @@
+print("hello")
+
+
+length_char=len("hello word")
+print(length_char)
+
+
+
+
+def myFunction():
+    print("Prashant")
+    print("sachan")
+
+myFunction()    
+
+
+
+# go to :
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json#
+
---
+++ udemyPython/day6/huddleLoopChalnage.py
@@ -0,0 +1,33 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def cycle():
+#   move()
+#   turn_left()
+#   move()
+#   turn_right()
+#   move()
+#   turn_right()
+#   move()
+#   turn_left()
+    
+# for step in range(4):
+#     cycle()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
